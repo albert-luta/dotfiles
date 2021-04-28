93b107668dcc0edd77371f4fe531e6cf5e98cab3
Albert Luta <luta.albert100@gmail.com>
2021-04-28 03:20:40 +0300

    Reorganized root files

--- init/symlinks.sh
+++ init/symlinks.sh
@@ -2,14 +2,14 @@
 
 path=$(dirname $(dirname $(readlink -f $0)))
 
-# Xorg
-ln -sf $path/.xinitrc $HOME/.xinitrc
-ln -sf $path/.xprofile $HOME/.xprofile
+# xorg
+ln -sf $path/xorg/.xinitrc $HOME/.xinitrc
+ln -sf $path/xorg/.xprofile $HOME/.xprofile
 
-# Git
-ln -sf $path/.gitconfig $HOME/.gitconfig
+# git
+ln -sf $path/git/.gitconfig $HOME/.gitconfig
 
-# Alacritty
+# alacritty
 [ ! -d $HOME/.config/alacritty ] && mkdir -p $HOME/.config/alacritty
 ln -sf $path/alacritty/* $HOME/.config/alacritty/
 
