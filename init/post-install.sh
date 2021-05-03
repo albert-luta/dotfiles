79758d385345276771cfc95c2e09f3957a424732
Albert Luta <luta.albert100@gmail.com>
2021-05-03 23:35:14 +0300

    Automatically switch to ssh after installation

--- init/post-install.sh
+++ init/post-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Switch dotfiles repo connection from https to ssh
+git remote set-url origin git@github.com:lutaalbert/dotfiles.git 
+yes | git push -u origin master
+
 # Reload zsh to install the remaining packages
 . $HOME/.bashrc # Wsl
 # . $HOME/.zshrc # Native Ubuntu
