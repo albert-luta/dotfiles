d3c134a4af0e84f99d9a47805547c5b88f1d8bcc
lutaalbert <55731599+lutaalbert@users.noreply.github.com>
2021-05-30 16:16:33 +0300

    Update installs.sh

--- fonts/installs.sh
+++ fonts/installs.sh
@@ -1,9 +1,8 @@
 [ ! -d $HOME/.local/share/fonts ] && mkdir -p $HOME/.local/share/fonts
 
 # hack
-curl -fL https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip --create-dirs -o $download/fonts/hack.zip
+curl -fL "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip" --create-dirs -o $download/fonts/hack.zip
 unzip $download/fonts/hack.zip  -d $HOME/.local/share/fonts/hack/
 
 # Rebuild the font cache
 fc-cache -fv
-
