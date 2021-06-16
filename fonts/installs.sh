3c4480dc9f2d1d8057f2043d1b12fbda3080a3a2
Albert Luta <luta.albert100@gmail.com>
2021-06-16 23:13:14 +0300

    Fixed the font installation problem

--- fonts/installs.sh
+++ fonts/installs.sh
@@ -3,8 +3,8 @@
 downloads=$HOME/Downloads
 
 # hack
-curl -fL "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip" --create-dirs -o $download/fonts/hack.zip
-unzip $download/fonts/hack.zip  -d $HOME/.local/share/fonts/hack/
+curl -fL "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip" --create-dirs -o $downloads/fonts/hack.zip
+unzip $downloads/fonts/hack.zip  -d $HOME/.local/share/fonts/hack/
 
 # Rebuild the font cache
 fc-cache -fv
