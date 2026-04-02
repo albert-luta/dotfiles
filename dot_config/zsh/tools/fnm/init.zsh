a845b44e7841512ffdc33f9a834fcaac07be4922
Albert Luta <github.7ntgj@simplelogin.co>
2026-04-02 05:50:20 +0300

    fix: supress fnm node version message on shell init

--- dot_config/zsh/tools/fnm/init.zsh
+++ dot_config/zsh/tools/fnm/init.zsh
@@ -1,5 +1,5 @@
 # add env vars, auto load .nvmrc when switching directories, enable corepack when installing new node versions
-eval "$(fnm env --use-on-cd --corepack-enabled)"
+eval "$(fnm env --log-level quiet --use-on-cd --corepack-enabled)"
 
 # add completions
 source $HOME/.config/zsh/tools/fnm/_fnm
